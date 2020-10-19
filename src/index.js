d7dc9ea8b4514ae4a46b1fb98f915e7a90560415
Diana Martyn <dianamartin790@gmail.com>
2020-10-20 01:51:59 +0300

    feat: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let numbers = n.toString().split("");
+    let res = "";
+    for (let i = numbers.length - 1; i >= 0; i--) {
+        res += numbers[i] == '-' ? '' : numbers[i];
+    }
+    return res;
 }
